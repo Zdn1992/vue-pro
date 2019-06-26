78a2ba43137306abc940acb6c17b2f4f13c3c0ce
Zdn1992 <215585990@qq.com>
2019-06-26 17:48:02 +0800

    tabbar

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # heima Vue项目
 
 ## study study study
+
+## 制作首页App组件
+
+- 完成Header区域, 使用的是Mint-UI的Header组件
+
+- 制作底部的Tabbar 区域,使用的是MUI的Tabbar.html
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 
 // 导入MUI的样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 // 按需导入Mint-UI的组件
 import { Header } from 'mint-ui';
