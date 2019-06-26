02e4e3dd02196eb9f5cfec6c7fd4f7d56b978aca
Zdn1992 <215585990@qq.com>
2019-06-26 17:06:45 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,3 @@
+# 学习黑马程序员的Vue项目
+
+## study study study
---
+++ src/main.js
@@ -0,0 +1,15 @@
+// 入口文件
+import Vue from 'vue'
+import App from './App.vue'
+
+// 导入MUI的样式
+import './lib/mui/css/mui.min.css'
+
+// 按需导入Mint-UI的组件
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+var vm = new Vue({
+    el:"#app",
+    render:c => c(App),
+})
